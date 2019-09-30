d216e761e89a0ad421af6b34417ebede01ca4d48
Justin <Pashweetie@gmail.com>
2019-09-30 13:47:55 -0600

    its totally complete!

--- timmy3.py
+++ timmy3.py
@@ -1,8 +1,11 @@
+from pynput.keyboard import Key, Listener
+import getpass
 class Node:
   def __init__(self, cargo, children=None, isWord = False):
     self.isWord = isWord
     self.cargo = cargo
     self.children = [None]*26
+keyvalue = ''
 counter = 0
 resulting_words = []
 root = Node(None)
@@ -39,8 +42,9 @@ def traverse(node,word):
     global counter
     global resulting_words
     if  node.isWord == True:        
-        print('word: '+word)
-        resulting_words.append(word)            
+        # print(word)
+        if len(resulting_words) <10:
+          resulting_words.append(word)            
         counter = counter+1        
         for nodeys in node.children:            
             if nodeys != None and counter<10:
@@ -50,30 +54,94 @@ def traverse(node,word):
             if nodey != None:                
                 traverse(nodey,word+nodey.cargo)
 
+def on_press(key):
+    # print('{0} pressed'.format(
+    #     key))
+    global keyvalue
+    # print('key entered: {0}'.format(key))
+    if key == Key.backspace:
+      keyvalue = keyvalue[:-1]
+      print()
+      print()
+      print()
+      print('your input: '+keyvalue)
+      if keyvalue != '':    
+        run_traverse(keyvalue)
+    elif key.char.isalpha():      
+      keyvalue = keyvalue + '{0}'.format(key.char)
+      print()
+      print()
+      print()
+      print('your input: '+keyvalue)
+      if keyvalue != '':    
+        run_traverse(keyvalue)
+    else:
+      print('invalid char')
+    
+def on_release(key):
+    # print('{0} release'.format(
+    #     key))
+    
+    if key == Key.esc:
+        # Stop listener
+        return False
+def run_traverse(test):
+  global root
+  global counter
+  global resulting_words
+  baseNode=root
+  # print('in key listener')
+  counter = 0
+  baseNode = find_word(test.lower(),baseNode)
+  print('Suggestions: ')
+  if baseNode!=None:        
+      traverse(baseNode,test.lower())
+  else:
+      print('value does not return any suggestions')
+  # print(resulting_words)
+  for word in resulting_words:
+      print(word)
+  resulting_words = []
+
 def main():
   global root
   global counter
+  global keyvalue
   f= open('words.txt','r')
+  # Collect events until released
+  
   root = Node(None)
   fline = f.readlines()
   for line in fline:
     root = build(root, line.lower().rstrip())
   f.close()
   baseNode=root
-  print('Done with tree building')
+  print('Done with tree building')  
+  print('go ahead and input your word, esc to exit')
+  # with Listener(
+  #         on_press=on_press,
+  #         on_release=on_release) as listener:
+  #     listener.join()
+  listener = Listener(
+    on_press=on_press,
+    on_release=on_release)
+  listener.start()
   global resulting_words
-  while(True):
-    test = input("Enter a word: ")
-    baseNode=root
-    resulting_words = []
-    counter = 0
-    baseNode = find_word(test.lower(),baseNode)
-    if baseNode!=None:        
-        traverse(baseNode,test.lower())
-    else:
-        print('value does not return any suggestions')
-    for word in resulting_words:
-        print(word)
-    resulting_words = ''
+  while(True):    
+    test = getpass.getpass('')
+    # print ('in main')
+    # baseNode=root
+    # resulting_words = []
+    # counter = 0
+    # baseNode = find_word(test.lower(),baseNode)
+    # if baseNode!=None:        
+    #     traverse(baseNode,test.lower())
+    # else:
+    #     print('value does not return any suggestions')
+    # print()
+    # print('your input: '+ keyvalue)
+    # for word in resulting_words:
+    #     print(word)
+    # resulting_words = ''
 if __name__ == "__main__":
   main()
